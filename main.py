36cbcd326ec0a4cad6b4b9efc4afdfb5e4bdbb65
GiancarloMacchi <81302593+GiancarloMacchi@users.noreply.github.com>
2025-08-11 00:54:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
 from utils import cerca_prodotti, KEYWORDS
-import os
-import requests
 
-TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
-TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
-
-def invia_telegram(messaggio):
-    try:
-        url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-        payload = {"chat_id": TELEGRAM_CHAT_ID, "text": messaggio}
-        requests.post(url, data=payload)
-    except Exception as e:
-        print(f"Errore invio Telegram: {e}")
-
-def main():
-    for kw in KEYWORDS:
-        risultati = cerca_prodotti(kw)
-        for r in risultati:
-            invia_telegram(r)
-
-if __name__ == "__main__":
-    main()
+for parola in KEYWORDS:
+    prodotti = cerca_prodotti(parola)
+    print(f"Risultati per {parola}:")
+    for p in prodotti:
+        print(p)
