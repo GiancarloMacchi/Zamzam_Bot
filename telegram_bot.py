e289fe48a4a56c311f89bba2c6322c73dad2cd10
GiancarloMacchi <81302593+GiancarloMacchi@users.noreply.github.com>
2025-08-12 12:11:38 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,16 +1,35 @@
-import telegram
 import os
+from telegram import Bot
+from telegram.constants import ParseMode
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def get_telegram_client():
+    """
+    Restituisce il client Telegram Bot.
+    """
     if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
-        raise ValueError("Token o Chat ID di Telegram non impostati.")
-    return telegram.Bot(token=TELEGRAM_BOT_TOKEN)
+        raise ValueError("Token o Chat ID di Telegram non impostati nei secrets.")
+    return Bot(token=TELEGRAM_BOT_TOKEN)
 
 def send_telegram_message(bot, message):
-    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message, parse_mode=telegram.ParseMode.HTML)
+    """
+    Invia un messaggio di testo su Telegram.
+    """
+    bot.send_message(
+        chat_id=TELEGRAM_CHAT_ID,
+        text=message,
+        parse_mode=ParseMode.HTML
+    )
 
 def send_telegram_photo(bot, photo_url, caption):
-    bot.send_photo(chat_id=TELEGRAM_CHAT_ID, photo=photo_url, caption=caption, parse_mode=telegram.ParseMode.HTML)
+    """
+    Invia una foto con didascalia su Telegram.
+    """
+    bot.send_photo(
+        chat_id=TELEGRAM_CHAT_ID,
+        photo=photo_url,
+        caption=caption,
+        parse_mode=ParseMode.HTML
+    )
