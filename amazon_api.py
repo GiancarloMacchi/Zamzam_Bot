e66a488228f77672916de3a871a4e12131fe4015
GiancarloMacchi <81302593+GiancarloMacchi@users.noreply.github.com>
2025-08-12 11:05:45 +0200

    Update amazon_api.py

--- amazon_api.py
+++ amazon_api.py
@@ -1,24 +1,15 @@
 from amazon_paapi5 import AmazonApi
-
 import os
 
-
-
 AMAZON_ACCESS_KEY = os.getenv("AMAZON_ACCESS_KEY")
-
 AMAZON_SECRET_KEY = os.getenv("AMAZON_SECRET_KEY")
-
 AMAZON_ASSOCIATE_TAG = os.getenv("AMAZON_ASSOCIATE_TAG")
-
 AMAZON_COUNTRY = os.getenv("AMAZON_COUNTRY", "IT")
 
-
-
 def get_amazon_client():
-
-    return AmazonApi(
-        AMAZON_ACCESS_KEY,
-        AMAZON_SECRET_KEY,
-        AMAZON_ASSOCIATE_TAG,
-        AMAZON_COUNTRY)
-
+    return AmazonApi(
+        AMAZON_ACCESS_KEY,
+        AMAZON_SECRET_KEY,
+        AMAZON_ASSOCIATE_TAG,
+        AMAZON_COUNTRY
+    )
