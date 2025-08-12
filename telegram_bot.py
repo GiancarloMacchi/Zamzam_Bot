37aa4f7d11ce0bd0801193636afbd0be3c0e6790
GiancarloMacchi <81302593+GiancarloMacchi@users.noreply.github.com>
2025-08-12 10:53:39 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,9 +1,16 @@
+import telegram
 import os
-from telegram import Bot
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-def invia_messaggio(messaggio):
-    bot = Bot(token=TELEGRAM_BOT_TOKEN)
-    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=messaggio)
+def get_telegram_client():
+    if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
+        raise ValueError("Token o Chat ID di Telegram non impostati.")
+    return telegram.Bot(token=TELEGRAM_BOT_TOKEN)
+
+def send_telegram_message(bot, message):
+    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message, parse_mode=telegram.ParseMode.HTML)
+
+def send_telegram_photo(bot, photo_url, caption):
+    bot.send_photo(chat_id=TELEGRAM_CHAT_ID, photo=photo_url, caption=caption, parse_mode=telegram.ParseMode.HTML)
