3d5617099aa66668a34b0af71d86b7b403c2a262
GiancarloMacchi <81302593+GiancarloMacchi@users.noreply.github.com>
2025-08-13 16:50:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-import logging
 from amazon_client import get_items
 from telegram_bot import send_telegram_message
 import os
 from dotenv import load_dotenv
 
-logging.basicConfig(level=logging.INFO)
-
 load_dotenv()
 
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
 def main():
-    logging.info("🔍 Recupero articoli da Amazon...")
-    items = get_items()
+    send_telegram_message("🔍 Recupero articoli da Amazon...")
 
+    items = get_items()
     if not items:
-        send_telegram_message(
-            TELEGRAM_CHAT_ID,
-            "❌ Nessuna offerta trovata o errore nelle API Amazon."
-        )
+        send_telegram_message("❌ Nessuna offerta trovata o errore nelle API Amazon.")
         return
 
     for item in items:
-        message = f"📦 {item['title']}\n💰 {item['price']} {item['currency']}\n💸 Sconto: {item['saving']}%\n🔗 {item['url']}"
-        send_telegram_message(TELEGRAM_CHAT_ID, message)
+        message = (
+            f"📦 <b>{item['title']}</b>\n"
+            f"💰 Prezzo: {item['price']} {item['currency']}\n"
+            f"💸 Sconto: {item['saving']}%\n"
+            f"🔗 <a href='{item['url']}'>Acquista ora</a>"
+        )
+        send_telegram_message(message)
 
 if __name__ == "__main__":
     main()
