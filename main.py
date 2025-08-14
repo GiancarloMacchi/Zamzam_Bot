a77cb1da15837daef851496721dc5f7218034ffa
GiancarloMacchi <81302593+GiancarloMacchi@users.noreply.github.com>
2025-08-14 10:31:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
-import os
 import logging
-from amazon_client import get_items
+from amazon_client import AmazonClient
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-def main():
-    logger.info("🔍 Recupero articoli da Amazon...")
-
-    # Recupero le keywords dalle Secret
-    keywords_env = os.getenv("KEYWORDS")
-    if not keywords_env:
-        logger.error("❌ Variabile KEYWORDS non trovata nelle Secret")
-        return
-
-    keywords = [k.strip() for k in keywords_env.split(",") if k.strip()]
-    if not keywords:
-        logger.error("❌ Nessuna keyword valida trovata")
-        return
-
-    totale_articoli = 0
-
-    for keyword in keywords:
-        items = get_items(keyword)
-        if items:
-            totale_articoli += len(items)
-        else:
-            logger.warning(f"⚠️ Nessun articolo trovato per '{keyword}'")
-
-    logger.info(f"ℹ️ Trovati {totale_articoli} articoli in totale.")
 
 if __name__ == "__main__":
-    main()
+    logging.info("🔍 Recupero articoli da Amazon...")
+    client = AmazonClient()
+
+    # Solo 1 keyword di test
+    keyword = "lego"
+    items = client.search_items(keyword, item_count=5)
+
+    logging.info(f"📦 Risultati trovati per '{keyword}': {len(items)}")
+    for item in items:
+        logging.info(item)
