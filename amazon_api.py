38a4b7a4f9d263a08f64fc994fc62a304098199f
GiancarloMacchi <81302593+GiancarloMacchi@users.noreply.github.com>
2025-08-17 16:51:24 +0200

    Update amazon_api.py

--- amazon_api.py
+++ amazon_api.py
@@ -1,27 +1,28 @@
 import logging
-import time
-from amazon_paapi import AmazonAPI  # <--- correggiamo l'import
-from config import load_config
+from amazon_paapi import AmazonAPI
 
 def search_amazon(keyword, config):
-    client = AmazonAPI(
-        access_key=config['AMAZON_ACCESS_KEY'],
-        secret_key=config['AMAZON_SECRET_KEY'],
-        associate_tag=config['AMAZON_ASSOCIATE_TAG'],
-        country=config['AMAZON_COUNTRY']
-    )
-
     try:
+        client = AmazonAPI(
+            access_key=config['AMAZON_ACCESS_KEY'],
+            secret_key=config['AMAZON_SECRET_KEY'],
+            associate_tag=config['AMAZON_ASSOCIATE_TAG'],
+            country=config['AMAZON_COUNTRY']
+        )
         products = client.search_products(keywords=keyword, item_count=int(config['ITEM_COUNT']))
         results = []
         for p in products:
-            results.append({
-                "title": p.title,
-                "url": p.detail_page_url,
-                "price": p.price_and_currency[0] if p.price_and_currency else "N/A",
-                "image": p.large_image_url,
-                "description": p.title  # placeholder, puoi personalizzare
-            })
+            if p.price_and_currency and p.original_price and p.original_price[0] > 0:
+                discount_percentage = ((p.original_price[0] - p.price_and_currency[0]) / p.original_price[0]) * 100
+                if discount_percentage >= int(config['MIN_SAVE']):
+                    results.append({
+                        "title": p.title,
+                        "url": p.detail_page_url,
+                        "price": p.price_and_currency[0],
+                        "original_price": p.original_price[0],
+                        "image": p.large_image_url,
+                        "discount": int(discount_percentage)
+                    })
         return results
     except Exception as e:
         logging.warning(f"Errore API Amazon: {e}")
