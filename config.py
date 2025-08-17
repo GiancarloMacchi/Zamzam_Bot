aee3a765cd0d3368a8a861fd3031dd973b66ca80
GiancarloMacchi <81302593+GiancarloMacchi@users.noreply.github.com>
2025-08-17 14:14:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ def load_config():
     with open("config.json", "r") as f:
         config = json.load(f)
     
-    # Sovrascrivi con le secrets / env variables se presenti
+    # Sovrascrivi con secrets/env variables se presenti
     config["AMAZON_ACCESS_KEY"] = os.getenv("AMAZON_ACCESS_KEY", config.get("AMAZON_ACCESS_KEY"))
     config["AMAZON_SECRET_KEY"] = os.getenv("AMAZON_SECRET_KEY", config.get("AMAZON_SECRET_KEY"))
     config["AMAZON_ASSOCIATE_TAG"] = os.getenv("AMAZON_ASSOCIATE_TAG", config.get("AMAZON_ASSOCIATE_TAG"))
