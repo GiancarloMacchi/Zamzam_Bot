9f7c629157319cc31baf27573ad59535ccb6e5c6
GiancarloMacchi <81302593+GiancarloMacchi@users.noreply.github.com>
2025-08-17 16:52:02 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,15 +1,13 @@
 import logging
-from telegram import Bot
-
+import telegram
 
 def send_telegram_message(config, message: str):
     """
     Invia un messaggio al canale Telegram configurato.
-    Usa il bot token e chat id definiti nelle variabili di ambiente / config.
     """
     try:
-        bot = Bot(token=config["TELEGRAM_BOT_TOKEN"])
-        bot.send_message(chat_id=config["TELEGRAM_CHAT_ID"], text=message)
+        bot = telegram.Bot(token=config["TELEGRAM_BOT_TOKEN"])
+        bot.send_message(chat_id=config["TELEGRAM_CHAT_ID"], text=message, parse_mode='Markdown')
         logging.info("Messaggio inviato su Telegram")
     except Exception as e:
         logging.error(f"Errore nell'invio del messaggio Telegram: {e}")
