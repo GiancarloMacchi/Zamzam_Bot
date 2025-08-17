4184d92bc4ad9aa9e344cfd39608e3e6b0ef63f7
GiancarloMacchi <81302593+GiancarloMacchi@users.noreply.github.com>
2025-08-17 13:57:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 import json
+import os
 
 def load_config():
     with open("config.json", "r") as f:
-        return json.load(f)
+        config = json.load(f)
+    
+    # Sovrascrivi con le secrets / env variables se presenti
+    config["AMAZON_ACCESS_KEY"] = os.getenv("AMAZON_ACCESS_KEY", config.get("AMAZON_ACCESS_KEY"))
+    config["AMAZON_SECRET_KEY"] = os.getenv("AMAZON_SECRET_KEY", config.get("AMAZON_SECRET_KEY"))
+    config["AMAZON_ASSOCIATE_TAG"] = os.getenv("AMAZON_ASSOCIATE_TAG", config.get("AMAZON_ASSOCIATE_TAG"))
+    config["TELEGRAM_BOT_TOKEN"] = os.getenv("TELEGRAM_BOT_TOKEN", config.get("TELEGRAM_BOT_TOKEN"))
+    config["TELEGRAM_CHAT_ID"] = os.getenv("TELEGRAM_CHAT_ID", config.get("TELEGRAM_CHAT_ID"))
+    
+    return config
