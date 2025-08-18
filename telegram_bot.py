9bea409f68c1908fcb7b497e3b1b89f9bf75468b
GiancarloMacchi <81302593+GiancarloMacchi@users.noreply.github.com>
2025-08-18 14:11:17 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,30 @@
-import logging
 import telegram
+import logging
+import time
 
-def send_telegram_message(config, message: str):
-    """
-    Invia un messaggio al canale Telegram configurato.
-    """
+def send_telegram_message(config, products_list, keyword):
     try:
+        if not products_list:
+            logging.info(f"Nessun prodotto trovato per: {keyword}")
+            return
+        
         bot = telegram.Bot(token=config["TELEGRAM_BOT_TOKEN"])
+        
+        message = f"**🔥 Super Offerte su Amazon - {keyword.capitalize()} 🔥**\n\n"
+        
+        for p in products_list:
+            message += f"**{p['title']}**\n"
+            message += f"Sconto: {p['discount']}%\n"
+            message += f"Prezzo: {p['price']}€\n"
+            message += f"Link: {p['url']}\n\n"
+        
+        logging.debug(f'Set Bot API URL: {bot.base_url}')
+        logging.debug(f'Set Bot API File URL: {bot.base_file_url}')
+
         bot.send_message(chat_id=config["TELEGRAM_CHAT_ID"], text=message, parse_mode='Markdown')
+        
         logging.info("Messaggio inviato su Telegram")
+        time.sleep(5)
+        
     except Exception as e:
-        logging.error(f"Errore nell'invio del messaggio Telegram: {e}")
+        logging.error(f"Errore durante l'invio del messaggio Telegram: {e}")
